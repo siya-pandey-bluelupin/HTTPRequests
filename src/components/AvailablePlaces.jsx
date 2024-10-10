861c1f000174c432df1b6c271bde22888400e283
siya-pandey-bluelupin <siya@bluelupin.com>
2024-10-10 13:50:05 +0530

    complete app with backend

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import Places from './Places.jsx';
+import ErrorPage from './ErrorPage.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import {fetchAvailablePlaces} from '../http.js';
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+
+     const places=  await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position)=>{
+const sortedPlaces=sortPlacesByDistance(places,position.coords.latitude,position.coords.longitude)
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+
+        });
+        
+
+      } catch (error) {
+        setError({message:error.message ||'Could not fetch places, please try again later'});
+        setIsFetching(false);
+
+      }
+    }
+    fetchPlaces();
+  }
+    , []);
+    if (error){
+      return <ErrorPage title="An Error occured !" message={error.message}/>
+    }
+
+
+
+
+
+
+  
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place Data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
