31eb3c1b7d59e78d481450346f6248e80ef8a81b
siya-pandey-bluelupin <siya@bluelupin.com>
2024-10-10 17:39:56 +0530

    add custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef, useState, useCallback, } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
@@ -7,33 +7,15 @@ import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { fetchUserPlaces, updateUserPlaces } from './http.js';
 import ErrorPage from './components/ErrorPage.jsx';
-
+import { useFetch } from './hooks/useFetch.js';
 function App() {
   const selectedPlace = useRef();
 
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
+
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-
-useEffect(() => {
-  async function fetchPlaces() {
-    setIsFetching(true);
-    try {
-      const places=await fetchUserPlaces();
-      setUserPlaces(places);
-    } catch (error) {
-      setError({message:error.message||' Failed to fetch user places '})
-    }
-  setIsFetching(false);
-  }
-
- 
-  fetchPlaces();
-}, [])
-
+  const { isFetching, error, fetchedData: userPlaces, setFetchedData: setUserPlaces, } = useFetch(fetchUserPlaces, []);
 
 
 
@@ -73,17 +55,18 @@ useEffect(() => {
     );
     try {
       await updateUserPlaces(
-        userPlaces.filter((place)=>place.id !==selectedPlace.current.id)
+        userPlaces.filter((place) => place.id !== selectedPlace.current.id)
       );
-      
+
     } catch (error) {
       setUserPlaces(userPlaces);
       setErrorUpdatingPlaces({
-        message:error.message || 'Failed to delete place.'
+        message: error.message || 'Failed to delete place.'
       })
     }
     setModalIsOpen(false);
-  }, [userPlaces]);
+  }, [userPlaces, setUserPlaces]
+  );
 
 
   function handleError() {
@@ -101,7 +84,9 @@ useEffect(() => {
           />
         )}
       </Modal>
-      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+      <Modal open={modalIsOpen}
+        onClose={handleStopRemovePlace}
+      >
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
@@ -117,21 +102,23 @@ useEffect(() => {
         </p>
       </header>
       <main>
-      {error && <ErrorPage title="An error occured ! " message={error.message}/>}
-       {
-        !error && (
-          <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-         isLoading={isFetching}
-         loadingText="Fetching your places"
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        )
-       }
+        {error && <ErrorPage title="An error occured ! " message={error.message} />}
+        {
+          !error && (
+            <Places
+              title="I'd like to visit ..."
+              fallbackText="Select the places you would like to visit below."
+              isLoading={isFetching}
+              loadingText="Fetching your places"
+              places={userPlaces}
+              onSelectPlace={handleStartRemovePlace}
+            />
+          )
+        }
 
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+        <AvailablePlaces
+          onSelectPlace={handleSelectPlace}
+        />
       </main>
     </>
   );
